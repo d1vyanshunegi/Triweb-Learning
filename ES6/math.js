76ed10e14184430ba374724abb8bec1caf46e873
Divyanshu Negi <divyanshunegi1@gmail.com>
2023-08-24 19:22:15 +0530

    Practiced global methods isNan and isFinite in ES6

--- ES6/math.js
+++ ES6/math.js
@@ -24,3 +24,7 @@ let b = 9007199254740992
 console.log(Number.isInteger(b));
 
 console.log("Safe integer",Number.isSafeInteger(b));
+
+console.log("isFinite",isFinite(20000/0));
+
+console.log("isNaN", isNaN(2/'e'));
