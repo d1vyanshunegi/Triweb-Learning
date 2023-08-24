697bd5bc6144a726e235f76cb444e4a1a991293f
Divyanshu Negi <divyanshunegi1@gmail.com>
2023-08-24 15:31:48 +0530

    Practiced class in ES6

---
+++ ES6/class.js
@@ -0,0 +1,15 @@
+class Student{
+    constructor(sname, classId){
+        this.sname = sname;
+        this.classId = classId;
+    }
+}
+
+
+const student1 = new Student("Divyanshu", 1);
+
+const student2 = new Student("Rohit", "5th");
+
+console.log(student1);
+
+console.log(student2);
