7e7ff3f99ca9be9f3ee5cfe8f37bf19b04fd4f0e
Divyanshu Negi <divyanshunegi1@gmail.com>
2023-08-24 19:19:22 +0530

    Practiced different number methods in ES6

--- ES6/math.js
+++ ES6/math.js
@@ -8,3 +8,19 @@ console.log(Math.cbrt(64));
 
 console.log(Math.log2(2));
 console.log(Math.log10(10));
+
+let x = Number.EPSILON;
+console.log(x);
+
+let y = Number.MAX_SAFE_INTEGER;
+console.log(y);
+
+let z = Number.MIN_SAFE_INTEGER;
+console.log(z);
+
+
+let a = 123;
+let b = 9007199254740992
+console.log(Number.isInteger(b));
+
+console.log("Safe integer",Number.isSafeInteger(b));
