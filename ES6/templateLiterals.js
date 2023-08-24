e1525bac7a677ab6893861a81d50bf4f04b691ca
Divyanshu Negi <divyanshunegi1@gmail.com>
2023-08-24 08:53:51 +0530

    Practiced binary and octal literals in ES6

--- ES6/templateLiterals.js
+++ ES6/templateLiterals.js
@@ -19,3 +19,10 @@ age = 20;
 something`Student's name is ${sname} and age is ${age}`;
 
 console.log(String.raw`age is \n ${40}` == "age is \\n 40")
+
+console.log(0b111110111 === 503)  //binary
+console.log(0o767 === 503)  //octal
+
+
+let num = parseInt('0111110111', 2);
+console.log(num == 503)
