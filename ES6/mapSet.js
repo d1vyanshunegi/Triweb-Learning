ea625f378ba0011ae56d7239a9dee68cb273d685
Divyanshu Negi <divyanshunegi1@gmail.com>
2023-08-24 15:22:49 +0530

    Practiced set in ES6

--- ES6/mapSet.js
+++ ES6/mapSet.js
@@ -9,3 +9,16 @@ studentMap.set(student1," He is excellent student");
 studentMap.set(student2, " He is good student");
 
 console.log(studentMap);
+
+
+let newSet = new Set();
+
+newSet.add(1);
+newSet.add(2);
+newSet.add(3);
+newSet.add(4);
+newSet.add(1);
+newSet.add(4);
+
+
+console.log(newSet);
