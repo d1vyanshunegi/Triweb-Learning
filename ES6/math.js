29b843c90f0a46a8781809258e3fe575ace6c499
Divyanshu Negi <divyanshunegi1@gmail.com>
2023-08-24 19:16:43 +0530

    Practiced math functions in ES6

---
+++ ES6/math.js
@@ -0,0 +1,10 @@
+console.log(Math.trunc(5.7));
+
+console.log(Math.sign(-19));
+console.log(Math.sign(20));
+console.log(Math.sign(0));
+
+console.log(Math.cbrt(64));
+
+console.log(Math.log2(2));
+console.log(Math.log10(10));
