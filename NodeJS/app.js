57c8e3ef2618d6d7ff613849b2cc3b3a90f1c538
Divyanshu Negi <divyanshunegi1@gmail.com>
2023-08-25 22:16:44 +0530

    Practiced concepts of GET and POST using express

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,3 +1,19 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+
+
+app.get('/', (req,res) => {
+    //console.log("Requested");
+    res.send("Hello");
+});
+
+app.post('/add', (req,res) => {
+    let n1 = req.body.num1;
+    let n2 = req.body.num2;
+    let sum = n1+n2;
+    console.log(req.body);
+    res.send(`Sum: ${sum}`);
+});
+
 app.listen(3000);
