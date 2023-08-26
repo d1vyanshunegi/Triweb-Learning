fdfa04780a0a0f56b73b1632b7c0f4b37a9ae179
Divyanshu Negi <divyanshunegi1@gmail.com>
2023-08-26 19:39:00 +0530

    Practiced async await CRUD operations on MySQL serrver using controller operations through model connectivity with database.

--- NodeJS/workshop2/models/user.js
+++ NodeJS/workshop2/models/user.js
@@ -1,13 +1,51 @@
 const db = require('../util/database');
 
 
-module.exports.insert = (userData) => {
-    console.log(userData);
-    console.log(userData.name);
+module.exports.insert = async (userData) => {
+    let id = 0;
+    try {
+        let query = "INSERT INTO `users`(`name`, `email`, `password`, `remark`) VALUES (?,?,?,?)";
+        let result = await db.execute(query,[userData.name, userData.email, userData.password, userData.remark]);
+        id = result[0].insertId;
+    } catch (error) {
+        console.log(error);
+    }
+    return id;
+}
 
-    let query = "INSERT INTO `users`(`name`, `email`, `password`, `remark`) VALUES (?,?,?,?);"
-    db.execute(query, [userData.name, userData.email, userData.password, userData.remark]);
+module.exports.get = async (userData)=>{
 
+    let retData = '';
+    try {
+        let query = "SELECT * FROM `users` WHERE id=?";
+        let result = await db.execute(query,[userData.id]);
+        retData = result[0][0];
+    } catch (error) {
+        console.log(error);
+    }
+    return retData;
+}
 
-    return true;
+module.exports.update = async (userData)=>{
+
+    try {
+        let query = "UPDATE `users` SET `password`=? WHERE id=?";
+        await db.execute(query,[userData.password, userData.id]);
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+module.exports.delete = async (userData)=>{
+
+    try {
+        let query = "DELETE FROM `users` WHERE id=?";
+        await db.execute(query,[userData.id]);
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
 }
