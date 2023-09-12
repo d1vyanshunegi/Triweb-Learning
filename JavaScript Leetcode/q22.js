d1d510c7270b677d96a56b9c30ba04c6f2ef3f98
Divyanshu Negi <divyanshunegi1@gmail.com>
2023-09-12 15:46:36 +0530

    Solved daily JavaScript leetcode question

---
+++ JavaScript Leetcode/q22.js
@@ -0,0 +1,10 @@
+// 242. Valid Anagram
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+var isAnagram = function(s, t) {
+    s=s.split("").sort().join("");
+    t=t.split("").sort().join("");
+    return (s==t);
+};
