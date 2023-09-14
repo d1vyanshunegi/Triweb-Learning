981b06c0c89ae9f094d397ecb984eafb2a3a3259
Divyanshu Negi <divyanshunegi1@gmail.com>
2023-09-14 11:18:35 +0530

    Created and set up a React project, added functional component, used props and useState hook

---
+++ React/dummy/src/App.js
@@ -0,0 +1,22 @@
+import FirstComponent from "./components/FirstComponent";
+import Backdrop from "./components/Backdrop";
+import {useState} from "react";
+
+function App() {
+
+  const [modalOpen, setModalOpen] = useState(true);
+
+  const modalCloseHandler = () => {
+    setModalOpen(false);
+  }
+
+  return (
+    <div className='modal'>
+      <h1>Heading</h1>
+      {modalOpen?<FirstComponent owner="Azhar" reason="For demo" closeModal={modalCloseHandler}/>:null}
+      {modalOpen?<Backdrop />:null}
+    </div>
+  );
+}
+
+export default App;
