f63ccd11573dd1cfa50f9b4e1db20bee6512fa91
Divyanshu Negi <divyanshunegi1@gmail.com>
2023-09-20 18:41:07 +0530

    Set up GET route for product and used useState and useEffect hooks to fetch product data from DB

--- React/my-project/src/pages/Product.js
+++ React/my-project/src/pages/Product.js
@@ -1,28 +1,56 @@
+import { useEffect, useState } from "react";
+
 import ProductList from "../components/product/ProductList";
 
-function Product(){
-    let products = [
-        {
-            id: 'p1',
-            product_name: "City Tour",
-            image:"https://images7.alphacoders.com/132/1327252.jpeg",
-            description: "Its a good tour",
-            price: "20000"
-        },
-        {
-            id: 'p2',
-            product_name: "Forest Tour",
-            image:"https://images4.alphacoders.com/131/1313274.jpeg",
-            description: "Its a refreshing tour",
-            price: "50000"
-        }
-    ]
+function Product() {
+    let [products, setProducts] = useState([]);
+    let [isDataFetching, setIsDataFetching] = useState(true);
 
-    return(
+    useEffect(() => {
+        setIsDataFetching(true);
+        fetch('http://127.0.0.1:3004/product')
+            .then(response => response.json())
+            .then(responseData => {
+                setIsDataFetching(false);
+                setProducts(responseData.data)
+            })
+            .catch(err => console.log(err));
+    }, [])
+
+
+    if (isDataFetching) {
+        return (
+            <div>
+                Loading . . .
+            </div>
+        )
+    };
+
+    return (
         <div>
-                <ProductList products={products} />
+            <ProductList products={products} />
         </div>
     )
 }
 
 export default Product;
+
+
+
+
+
+
+// {
+//     id: 'p1',
+//     product_name: "City Tour",
+//     image:"https://images7.alphacoders.com/132/1327252.jpeg",
+//     description: "Its a good tour",
+//     price: "20000"
+// },
+// {
+//     id: 'p2',
+//     product_name: "Forest Tour",
+//     image:"https://images4.alphacoders.com/131/1313274.jpeg",
+//     description: "Its a refreshing tour",
+//     price: "50000"
+// }
--- React/node-backend/app.js
+++ React/node-backend/app.js
@@ -21,6 +21,17 @@ app.post('/product', async (req,res)=>{
     }
 });
 
+app.get('/product', async (req,res)=>{
+    try {
+        console.log("Product fetching");
+        const products = await Product.find({});
+        res.send({status:'success', data:products});
+    } catch (error) {
+        console.log(error.message);
+        res.send({status:'error', message:error.message})
+    }
+})
+
 mongoose
     .connect(process.env.connectionString)
     .then(success=> app.listen(3004))
