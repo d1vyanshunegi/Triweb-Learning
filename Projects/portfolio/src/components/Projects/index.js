c4ca209c7688879fca0218a5fce0229337dfc7a1
Divyanshu Negi <divyanshunegi1@gmail.com>
2023-09-25 16:32:32 +0530

    Changed Project tickets layout and added css to scroll bar

--- Projects/portfolio/src/components/Projects/index.js
+++ Projects/portfolio/src/components/Projects/index.js
@@ -3,25 +3,29 @@ import './index.scss'
 const Projects = () => {
     return (
         <>
-            <h1 className='h1'>My Projects</h1>     
+            <h1 className='h1'>My Projects</h1>
+            <div className='center'>
+
                 <div className='scroll'>
-                    <span className='ticket'>
+                    <div className='ticket'>
                         <h2 className='head'>Quiz App Manager</h2>
-                            REST API architecture styled project to manage HTTP requests for performing CRUD operations like user registration, quiz creation, updation and deletion along with login authorization handeled using jwt on quiz app. Requests handeled using Postman.
+                        REST API architecture styled project to manage HTTP requests for performing CRUD operations like user registration, quiz creation, updation and deletion along with login authorization handeled using jwt on quiz app. Requests handeled using Postman. <br></br><br></br>
                         <a className='b' target="_blank" href="https://github.com/d1vyanshunegi/Triweb-Learning/tree/main/Projects/QuizManager">Link</a>
-                    </span>
-                    <span className='ticket'>
+                    </div>
+                    <div className='ticket'>
                         <h2 className='head'>Calci-Vanilla JS project</h2>
-                        
-                            A simple calculator made using HTML5, CSS3, and vanilla JavaScript!
-                        
+
+                        A simple calculator made using HTML5, CSS3, and vanilla JavaScript!<br></br><br></br>
+
                         <a className='b' target="_blank" href="https://divyanshunegicalci.netlify.app/">Link</a>
-                    </span>
-                    
-                    
+                    </div>
+
+
 
                 </div>
+            </div>
         </>
+
     )
 }
 
