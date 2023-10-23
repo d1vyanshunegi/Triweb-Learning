087d6ff4641459f591e7f7a72b263ca8bb1f146e
Divyanshu Negi <divyanshunegi1@gmail.com>
2023-10-23 19:48:38 +0530

    Implemented and practiced various types of promise methods

---
+++ NodeJS/NodeJS_InterviewQuestions/promise/promiseAny.js
@@ -0,0 +1,27 @@
+function fun1() {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        res("Function1");
+      }, 1000);
+    });
+  }
+  
+  function fun2() {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        res("Function2");
+      }, 2000);
+    });
+  }
+  
+  function fun3() {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        rej("Function3");
+      }, 500);
+    });
+  }
+  
+  Promise.any([fun1(), fun2(), fun3()])
+    .then((result) => console.log(result))
+    .catch((error) => console.log(error));
